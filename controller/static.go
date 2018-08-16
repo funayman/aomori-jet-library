da4cf54aad3b5577b1c1737fc5aeb064b4bf6f51
Dave Derderian <funayman@gmail.com>
2018-08-16 14:55:04 +0900

    implement search on index closes #2
    * created custom list.js to include stylings for bootstrap
    (https://github.com/funayman/list.js)

--- controller/static.go
+++ controller/static.go
@@ -5,6 +5,7 @@ import "github.com/funayman/aomori-library/router"
 func init() {
 	router.RouteStatic("/js/", "www/js")
 	router.RouteStatic("/css/", "www/css")
+	router.RouteStatic("/assets/", "www/assets")
 	// router.Instance().Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js/"))))
 	// router.Instance().PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("www/js/"))))
 }
---
+++ www/js/search.js
@@ -0,0 +1,8 @@
+// (function(id, opts) {
+//   var l = new List(id, opts)
+// })('books-list', { valueNames: ['title', 'author'], page: 2, pagination: true })
+
+var l = new List('books-list', { valueNames: ['title', 'author'], page: 2, pagination: true })
+// $('.pagination li').each(function() { li = $(this); li.addClass('page-item'); li.find('a').each(function(){$(this).addClass('page-link')}) })
+
+
