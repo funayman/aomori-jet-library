c9f49a6b4b74bb7d8c182fc3b9073b9f153173e3
Dave Derderian <funayman@gmail.com>
2018-08-17 09:36:33 +0900

    remove old code; use max cores when running

--- main.go
+++ main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"github.com/funayman/aomori-library/client"
+	"runtime"
+
 	"github.com/funayman/aomori-library/cmd"
 )
 
-var clients []client.Client
-
 func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
 	cmd.Execute()
-
-	/*
-		for _, isbn := range []string{"9780545010221", "9784840240536", "3319300024", "0316769487"} {
-			//for _, isbn := range []string{"3319300024"} {
-			book := client.FindBookByIsbn(isbn)
-			db.SQL.Save(book)
-		}
-
-		var books []model.Book
-		fmt.Println("books in db:")
-		err := db.SQL.All(&books)
-		if err != nil {
-			log.Println("error retreiving books: ", err)
-		}
-		fmt.Printf("%+v\n", books)
-	*/
 }
