c0877a080357141ed09a294ff137b168ad3049fa
Dave Derderian <funayman@gmail.com>
2018-08-18 16:17:09 +0900

    dynamically load routes into map #7

--- controller/controller.go
+++ controller/controller.go
@@ -1,8 +1,53 @@
 package controller
 
 import (
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/funayman/aomori-library/model/book"
 )
+
+const (
+	templateDir = "www/tmpl"
+)
+
+var mT map[string]*template.Template
+
+func init() {
+	mT = make(map[string]*template.Template)
+
+	var templates []string
+	var required []string
+	err := filepath.Walk(templateDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			// paths = append(paths, path)
+			if filepath.Base(path)[0] == '_' {
+				required = append(required, path)
+			} else {
+				templates = append(templates, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, t := range templates {
+		fn := filepath.Base(t)
+		tm := template.Must(template.ParseFiles(append([]string{t}, required...)...))
+		if err != nil {
+			log.Fatal(err)
+		}
+		mT[fn] = tm
+	}
+
+}
 
 func Load() {
 	// Do nothing!
